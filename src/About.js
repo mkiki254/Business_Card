fbdafdd605b10c85bc1be049c1b8351bf70f1a30
mkiki254 <kariukicharles440@gmail.com>
2022-12-15 20:55:52 +0300

    Completed the about section

--- src/About.js
+++ src/About.js
@@ -1,5 +1,8 @@
 export default function About(){
     return(
-        <h1>About section goes here</h1>
+        <div>
+            <h3 className="about-title">About</h3>
+            <p className="about-content">I am a front-end developer who is passionate about web design. I love to create attractive and easy to use user-interfaces to enhance the user experience. I also like exploring new tools to make the design and developmnent process simpler and easier</p>
+        </div>
     )
 }
